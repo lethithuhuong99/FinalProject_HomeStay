6b31aa9d4a11808f2b6cb90c4ea1e14abb6db09f
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-03-18 16:45:01 +0700

    1832019

--- scripts/information.js
+++ scripts/information.js
@@ -11,8 +11,8 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  
+
   slides[slideIndex-1].style.display = "block";
-  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
